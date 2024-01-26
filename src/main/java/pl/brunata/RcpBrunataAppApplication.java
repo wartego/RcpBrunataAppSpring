7b35ec3814287c8efc76f1a9f8bc8fa1c3195225
Tomasz <wartego@wp.pl>
2024-01-26 17:33:39 +0100

    first commit

---
+++ src/main/java/pl/brunata/RcpBrunataAppApplication.java
@@ -0,0 +1,13 @@
+package pl.brunata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcpBrunataAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RcpBrunataAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/brunata/RcpBrunataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.brunata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcpBrunataAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
